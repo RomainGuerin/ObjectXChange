8b99c5dde9d09803ba8b4f964bee5bad1fa61e62
benribou <ribou.benjamin@gmail.com>
2025-02-25 10:07:29 +0100

    Add database SQL Lite

--- src/main/java/org/esiee/App.java
+++ src/main/java/org/esiee/App.java
@@ -1,13 +1,18 @@
 package org.esiee;
-
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        String url = "jdbc:sqlite:database.db";
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                System.out.println("Connexion à SQLite établie.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Erreur de connexion : " + e.getMessage());
+        }
     }
 }
